3f75855dce0bfd55fba7ce2404deac1d8b77afb9
Jeff Sharp <public@sharpjs.net>
2019-09-21 11:19:22 -0700

    Retarget tests to net48 and netstandard2.2

--- Make.ps1
+++ Make.ps1
@@ -74,8 +74,8 @@ function Main {
 
     if ($Test -or $Coverage) {
         Set-Location -LiteralPath "$AssemblyNameRoot.Tests"
-        Invoke-TestForTargetFramework net472
-        Invoke-TestForTargetFramework netcoreapp2.1
+        Invoke-TestForTargetFramework net48
+        Invoke-TestForTargetFramework netcoreapp2.2
     }
 } 
 
